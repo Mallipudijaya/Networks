fcbd09fad5baaeb40c969f1c3ada9ef3c278647c
Mallipudijaya <42345244+Mallipudijaya@users.noreply.github.com>
2019-08-02 11:24:44 +0530

    Add files via upload

---
+++ udp/UDP1.java
@@ -0,0 +1,103 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+class UDP1
+{
+ public static DatagramSocket clientsocket;
+ public static DatagramPacket dp;
+ public static BufferedReader br;
+ public static InetAddress ia;
+ public static byte buf[] = new byte[1024];
+ 
+ public static int cport = 222, sport = 555;
+ public static void main(String[] args) throws IOException
+ {long b2=System.currentTimeMillis();
+byte[] data = new byte[4];byte[] data1 = new byte[4];byte[] data2 = new byte[4];
+  clientsocket = new DatagramSocket(cport);
+  dp = new DatagramPacket(buf, buf.length);
+  br = new BufferedReader(new InputStreamReader(System.in));
+  ia = InetAddress.getLocalHost();
+  
+  System.out.println("A is Running...");
+  System.out.println("Type some text if u want to Quit type 'exit'.");
+   Random rand = new Random();
+    int n = rand.nextInt(3);
+    data[0]=(byte)(n);data[1]=9;data[2]=0;data[3]=1;
+for(int i=0;i<4;i++)
+System.out.println((int)data[i]);   
+String str2 = new String(data);
+System.out.println(str2);   
+ buf = str2.getBytes();
+System.out.println(buf);
+ for(int i=0;i<4;i++)
+System.out.println((int)buf[i]); 
+  while(true)
+  {
+   String str1 = new String(data);
+   System.out.println(str1);  
+   buf = str1.getBytes();
+   if(str1.equals("exit"))
+   {
+    System.out.println("Terminated..");
+    clientsocket.send(new DatagramPacket(buf,str1.length(),ia,sport));
+    break;
+   }
+   clientsocket.send(new DatagramPacket(buf,str1.length(), ia,sport));
+ 
+   clientsocket.receive(dp);
+   String str4 = new String(dp.getData(), 0, dp.getLength());
+   buf = str4.getBytes();
+    System.out.println("C said : " );
+for(int i=0;i<3;i++)
+System.out.println((int)buf[i]+" ");
+   if(((int)buf[0])==0)
+{
+   int a= (int)(buf[1]);
+  a--;
+  buf[1]=(byte)a;
+    data1[0]=2;data1[1]=buf[1];data1[2]=buf[2];data1[3]=buf[3];
+      data2[0]=2;data2[1]=buf[1];data2[2]=buf[2];data2[3]=buf[3];
+   String str5 = new String(data1);
+   buf = str5.getBytes();
+    clientsocket.send(new DatagramPacket(buf,str5.length(),ia,sport));
+   String str6 = new String(data2);
+   buf = str6.getBytes();
+   clientsocket.send(new DatagramPacket(buf,str6.length(),ia,333));
+}
+Random rand1 = new Random();
+    int n1 = rand1.nextInt(2);
+if((int)buf[0]==2)
+{
+if(n1==0){
+   int b= (int)(buf[1]);
+  b--;
+  buf[1]=(byte)b;}
+if((int)buf[1]>0)
+    {data1[0]=2;data1[1]=buf[1];data1[2]=buf[2];data1[3]=buf[3];
+   String str5 = new String(data1);
+   buf = str5.getBytes();
+    clientsocket.send(new DatagramPacket(buf,str5.length(),ia,sport));
+   }
+if((int)buf[1]==0)
+    {data1[0]=1;data1[1]=buf[1];data1[2]=buf[2];data1[3]=buf[3];
+   String str5 = new String(data1);
+   buf = str5.getBytes();
+    clientsocket.send(new DatagramPacket(buf,str5.length(),ia,sport));
+   }
+}
+if((int)buf[0]==1)
+{
+System.out.println((int)buf[3]+" "+(int)buf[2]+" "+ia);
+}
+long b1=System.currentTimeMillis();
+if((b1-b2)==5)
+{
+data1[0]=1;data1[1]=8;data1[2]=buf[2];data1[3]=buf[3];
+   String str5 = new String(data1);
+   buf = str5.getBytes();
+    clientsocket.send(new DatagramPacket(buf,str5.length(),ia,sport));
+}
+  
+  }
+ }
+}
---
+++ udp/UDP2.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+class UDP2
+{
+	public static DatagramSocket serversocket;
+	public static DatagramPacket dp;
+	public static BufferedReader br;
+	public static InetAddress ia;
+	public static byte buf[] = new byte[1024];
+	public static int cport = 333,sport=555;
+	public static void main(String[] args) throws IOException
+       {long b2=System.currentTimeMillis();
+byte[] data = new byte[4];byte[] data1 = new byte[4];byte[] data2 = new byte[4];
+         serversocket = new DatagramSocket(sport);
+         dp = new DatagramPacket(buf,buf.length);
+         br = new BufferedReader (new InputStreamReader(System.in));
+         ia = InetAddress.getLocalHost();
+ 
+         System.out.println("B is Running...");
+Random rand = new Random();
+    int n = rand.nextInt(3);
+    data[0]=(byte)(n);data[1]=9;data[2]=0;data[3]=2;
+         while(true)
+         {
+          serversocket.receive(dp);
+          String str2 = new String(dp.getData(), 0, dp.getLength());
+          if(str2.equals("exit"))
+          {
+           System.out.println("Terminated...");
+           serversocket.send(new DatagramPacket(buf,str2.length(), ia, cport));
+           break;
+          }
+          System.out.println("A said : " );
+for(int i=0;i<3;i++)
+System.out.println((int)buf[i]+" ");
+  if(((int)buf[0])==0)
+{
+   int a= (int)(buf[1]);
+  a--;
+  buf[1]=(byte)a;
+    data1[0]=2;data1[1]=buf[1];data1[2]=buf[2];data1[3]=buf[3];
+      data2[0]=2;data2[1]=buf[1];data2[2]=buf[2];data2[3]=buf[3];
+   String str5 = new String(data1);
+   buf = str5.getBytes();
+    serversocket.send(new DatagramPacket(buf,str5.length(),ia,sport));
+   String str6 = new String(data2);
+   buf = str6.getBytes();
+   serversocket.send(new DatagramPacket(buf,str6.length(),ia,222));
+}
+Random rand1 = new Random();
+    int n1 = rand1.nextInt(2);
+if((int)buf[0]==2)
+{
+if(n1==0){
+   int b= (int)(buf[1]);
+  b--;
+  buf[1]=(byte)b;}
+if((int)buf[1]>0)
+    {data1[0]=2;data1[1]=buf[1];data1[2]=buf[2];data1[3]=buf[3];
+   String str5 = new String(data1);
+   buf = str5.getBytes();
+    serversocket.send(new DatagramPacket(buf,str5.length(),ia,sport));
+   }
+if((int)buf[1]==0)
+    {data1[0]=1;data1[1]=buf[1];data1[2]=buf[2];data1[3]=buf[3];
+   String str5 = new String(data1);
+   buf = str5.getBytes();
+    serversocket.send(new DatagramPacket(buf,str5.length(),ia,sport));
+   }
+}
+if((int)buf[0]==1)
+{
+System.out.println((int)buf[3]+" "+(int)buf[2]+" "+ia);
+}
+long b1=System.currentTimeMillis();
+if((b1-b2)==5)
+{
+data1[0]=1;data1[1]=8;data1[2]=buf[2];data1[3]=buf[3];
+   String str5 = new String(data1);
+   buf = str5.getBytes();
+    serversocket.send(new DatagramPacket(buf,str5.length(),ia,sport));
+}
+          /*String str3 = new String(br.readLine());
+          buf = str3.getBytes();*/
+          serversocket.send(new DatagramPacket(buf,str2.length(), ia, cport));
+         }
+       }
+    }
